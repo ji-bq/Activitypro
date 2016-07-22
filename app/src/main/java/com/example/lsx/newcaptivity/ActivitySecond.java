c5f0696634735d2e7c881245cbee8bdb0a4cc03e
test <test@163.com>
2016-07-22 14:35:31 +0800

    activitylife

---
+++ app/src/main/java/com/example/lsx/newcaptivity/ActivitySecond.java
@@ -0,0 +1,16 @@
+package com.example.lsx.newcaptivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lsx on 2016/7/22.
+ */
+public class ActivitySecond extends AppCompatActivity {
+    private static final String TAG = "ActivitySecond";
+
+
+}
+
---
+++ app/src/main/java/com/example/lsx/newcaptivity/Activitylife.java
@@ -0,0 +1,60 @@
+package com.example.lsx.newcaptivity;
+
+import android.content.Intent;
+import android.sax.StartElementListener;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class Activitylife extends AppCompatActivity {
+    private static final String TAG = "Activitylife";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activitylife);
+        Log.d(TAG, "onCreate:oncreate开始执行了 ");
+        Button dial= (Button) findViewById(R.id.activity_activitylife_button);
+        dial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Activitylife.this,ActivitySecond.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: onstart开始执行了 ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: onrestart开始执行了");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: onstop开始执行了");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: onpause开始执行了");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: 开始执行了");
+    }
+
+}
+
